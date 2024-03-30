58172dae9faf408c50a231deb717db9d2883dd87
antony-ch <146710275+antony-ch@users.noreply.github.com>
2024-03-30 09:31:05 +0700

    first commit

---
+++ src/Data.js
@@ -0,0 +1,49 @@
+let shopItemsData = [{
+    id: "abcd",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/image-1.jpg"
+},{
+    id: "efgh",
+    name: "Jacket",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/image-2.jpg"
+},{
+    id: "ijkl",
+    name: "Sweater",
+    price: 60,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/image-3.jpg"
+},{
+    id: "mnop",
+    name: "Jeans Pants",
+    price: 80,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/image-4.jpg"
+},{
+    id: "qrst",
+    name: "Shoes",
+    price: 90,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/image-5.jpg"
+},{
+    id: "uvwx",
+    name: "Black Hat",
+    price: 20,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/image-6.jpg"
+},{
+    id: "yzab",
+    name: "Sunglasses",
+    price: 30,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/image-7.jpg"
+},{
+    id: "cdef",
+    name: "Flip Flops",
+    price: 10,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/image-8.jpg"
+}];
